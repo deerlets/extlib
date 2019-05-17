097fdff4a49a57dc585d1d1322845a1acf521b7b
yonzkon <yonzkon@gmail.com>
2019-05-17 07:15:49 +0000

    Basic layout commit

---
+++ src/extlog.h
@@ -0,0 +1,32 @@
+#ifndef __EXT_LOG_H
+#define __EXT_LOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum log_level {
+	LOG_LV_NONE = 0,
+	LOG_LV_DEBUG,
+	LOG_LV_INFO,
+	LOG_LV_NOTICE,
+	LOG_LV_WARN,
+	LOG_LV_ERROR,
+	LOG_LV_FATAL,
+};
+
+int log_set_level(int level);
+int log_message(int level, const char *format, ...);
+
+#define LOG_NONE(format, ...) log_message(LOG_LV_NONE, format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) log_message(LOG_LV_DEBUG, format, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) log_message(LOG_LV_INFO, format, ##__VA_ARGS__)
+#define LOG_NOTICE(format, ...) log_message(LOG_LV_NOTICE, format, ##__VA_ARGS__)
+#define LOG_WARN(format, ...) log_message(LOG_LV_WARN, format, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) log_message(LOG_LV_ERROR, format, ##__VA_ARGS__)
+#define LOG_FATAL(format, ...) log_message(LOG_LV_FATAL, format, ##__VA_ARGS__)
+
+#ifdef __cplusplus
+}
+#endif
+#endif
