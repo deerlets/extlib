24e2eb17292183e0f886137ae9350f3ef5fb3c74
yonzkon <yonzkon@gmail.com>
2019-05-17 08:10:48 +0000

    docs(extlog.h): Add some comments

--- src/extlog.h
+++ src/extlog.h
@@ -8,16 +8,20 @@ extern "C" {
 enum log_level {
 	LOG_LV_NONE = 0,
 	LOG_LV_DEBUG,
-	LOG_LV_INFO,
+	LOG_LV_INFO, /* default */
 	LOG_LV_NOTICE,
 	LOG_LV_WARN,
 	LOG_LV_ERROR,
 	LOG_LV_FATAL,
 };
 
+/* Set log level and return former log level. */
 int log_set_level(int level);
+
+/* Log message to stdout */
 int log_message(int level, const char *format, ...);
 
+/* Marcos wrapping log_level for convenient usage of log_message */
 #define LOG_NONE(format, ...) log_message(LOG_LV_NONE, format, ##__VA_ARGS__)
 #define LOG_DEBUG(format, ...) log_message(LOG_LV_DEBUG, format, ##__VA_ARGS__)
 #define LOG_INFO(format, ...) log_message(LOG_LV_INFO, format, ##__VA_ARGS__)
